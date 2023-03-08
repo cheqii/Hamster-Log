1da66acc0da6b2c407fc9f95b85ce2f28a28d8b4
qiqitaro <thanajan.saes@bumail.net>
2023-03-08 14:51:02 +0700

    Import hamster log
    - Hamster Movement
    - Slope Instantiate

---
+++ Assets/Script/GroundCheck.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private bool isGround = false;
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.CompareTag("ground"))
+        {
+            isGround = true;
+        }
+       
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("ground"))
+        {
+            isGround = false;
+        }
+    }
+
+    public bool GetIsGround()
+    {
+        return isGround;
+    }
+}
